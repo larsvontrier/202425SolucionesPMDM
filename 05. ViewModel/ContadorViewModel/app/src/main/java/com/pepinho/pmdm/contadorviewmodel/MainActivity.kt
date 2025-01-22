3fd10b267f43bfa6d6aae0d03d72857d48e40e5a
Pepinho <pepecalo@iessanclemente.net>
2025-01-22 17:15:21 +0100

    contadorViewModel

---
+++ 05. ViewModel/ContadorViewModel/app/src/main/java/com/pepinho/pmdm/contadorviewmodel/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.pepinho.pmdm.contadorviewmodel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import com.pepinho.pmdm.contadorviewmodel.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: ContadorViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.btIncrementar.setOnClickListener {
+            viewModel.incremantar()
+        }
+
+        binding.btDecrementar.setOnClickListener {
+            viewModel.decrementar()
+        }
+
+        lifecycleScope.launch {
+            viewModel.valor.collect { numero ->
+                binding.tvNumero.text = numero.toString()
+
+            }
+        }
+
+
+
+
+    }
+}
